447461b69a8b8d84def65c87a05f5355a38d61ed
avocadojesus <jgarbey@gmail.com>
2016-03-25 11:41:33 -0700

    correctly binds markdown reading

--- public/app/commands/manifesto.js
+++ public/app/commands/manifesto.js
@@ -1,9 +1,13 @@
 var React = require('react')
 var $ = window.jQuery = window.$ = require('jquery')
 var Ascii = require('../components/ascii')
+var LineBreak = require('../components/line-break')
+var Markdown = require('../components/markdown')
 
 module.exports = function(args) {
   return [
-    <Ascii key={0} value='Manifesto' font='block'/>
+    <Ascii key={0} value='Manifesto' font='block'/>,
+    <LineBreak key={1} />,
+    <Markdown file='/markdown/manifesto.md'/>
   ]
 }
---
+++ public/app/components/line-break/line-break.js
@@ -0,0 +1,12 @@
+var React = require('react')
+
+var LineBreak = React.createClass({
+  displayName: 'LineBreak',
+  render: function() {
+    return (
+      <div>==================================================================================</div>
+    )
+  }
+})
+
+module.exports = LineBreak
