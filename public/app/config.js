fae15e6e6021d6f5cdb064b2492fb9b8a1724f90
avocadojesus <jgarbey@gmail.com>
2016-03-26 11:52:56 -0700

    adds gen-member command

--- public/app/config.js
+++ public/app/config.js
@@ -1,3 +1,2 @@
-exports.members = ['sudo', 'avojeez0']
 exports.address = "6882 Bridgewater Huntington Beach, CA 92647"
-exports.time = "Every Saturday Morning at 11:00 am"
+exports.time = "Every Sunday Morning at 11:00 am"
--- public/app/members/sudo/index.js
+++ public/app/members/sudo/index.js
@@ -1,5 +1,4 @@
 exports.name = "sudo"
-exports.description = "the ball is always in my court these days"
+exports.description = "sudo"
 exports.bio = function() {
-  return 'I am the inventor of balls in mouths'
 }
