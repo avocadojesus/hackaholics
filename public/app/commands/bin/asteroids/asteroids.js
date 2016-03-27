29b8016f1923658247aedb7d8a4392853f949d15
avocadojesus <jgarbey@gmail.com>
2016-03-27 09:06:41 -0700

    initializes command and component structure

---
+++ public/app/commands/bin/asteroids/asteroids.js
@@ -0,0 +1,18 @@
+var React = require('react')
+var $ = window.jQuery = window.$ = require('jquery')
+var Ascii = require('../../../components/ascii')
+var Markdown = require('../../../components/markdown')
+var expectOption = require('../../../lib/command-helper').expectOption
+var expectOptionWithArgument = require('../../../lib/command-helper').expectOptionWithArgument
+
+exports.description = "a fun game for teh lawlz"
+exports.name = "asteroids"
+exports.execute = function(args) {
+  if (expectOption('-h', args) || expectOption('--help', args)) return this.help()
+}
+exports.help = function() {
+  return [
+    <Ascii key={0} value={this.name} font='bell'/>,
+    <Markdown file={'/app/commands/bin/' + this.name + '/help.md'}/>
+  ]
+}
