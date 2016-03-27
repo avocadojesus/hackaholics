c6302afd16e2b65c0e94edf78b9cc5edc15f3c2b
avocadojesus <jgarbey@gmail.com>
2016-03-26 19:50:50 -0700

    adds help to god command

--- public/app/commands/bin/clear/clear.js
+++ public/app/commands/bin/clear/clear.js
@@ -6,8 +6,6 @@ var expectOption = require('../../../lib/command-helper').expectOption
 var expectOptionWithArgument = require('../../../lib/command-helper').expectOptionWithArgument
 var CommandActions = require('../../../actions/command-actions')
 
-
-
 exports.description = 'Clears your console window'
 exports.name = 'clear'
 exports.execute = function(args) {
@@ -16,7 +14,7 @@ exports.execute = function(args) {
 }
 exports.help = function() {
   return [
-    <Ascii key={0} value='clear' font='bell'/>,
+    <Ascii key={0} value={this.name} font='bell'/>,
     <Markdown file={'/app/commands/bin/' + this.name + '/help.md'}/>
   ]
 }
--- public/app/commands/bin/god/god.js
+++ public/app/commands/bin/god/god.js
@@ -1,7 +1,19 @@
-// var config = require('../../config.js')
 var React = require('react')
+var $ = window.jQuery = window.$ = require('jquery')
+var Ascii = require('../../../components/ascii')
+var Markdown = require('../../../components/markdown')
+var expectOption = require('../../../lib/command-helper').expectOption
+var expectOptionWithArgument = require('../../../lib/command-helper').expectOptionWithArgument
+
 exports.name = "god"
 exports.description = "gives you the answer to the ultimate question: who is god"
-exports.execute = function() {
+exports.execute = function(args) {
+  if (expectOption('-h', args) || expectOption('--help', args)) return this.help()
   return <img src='https://thesilentmajority.files.wordpress.com/2014/07/440px-pink_triangle_up-svg.png' />
+}
+exports.help = function() {
+  return [
+    <Ascii key={0} value={this.name} font='bell'/>,
+    <Markdown file={'/app/commands/bin/' + this.name + '/help.md'}/>
+  ]
 }
