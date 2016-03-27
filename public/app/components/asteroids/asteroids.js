1bdf7596b2bfa81c791b7fbbf0a964cfcb95859a
avocadojesus <jgarbey@gmail.com>
2016-03-27 10:11:59 -0700

    resets environment

--- public/app/components/asteroids/asteroids.js
+++ public/app/components/asteroids/asteroids.js
@@ -1,37 +1,4 @@
 var React = require('react')
-var Phaser = require('./phaser.min.js')
-
-var gameProperties = {
-    screenWidth: 640,
-    screenHeight: 480,
-};
-
-var states = {
-    game: "game",
-};
-
-var gameState = function(game){
-
-};
-
-gameState.prototype = {
-
-    preload: function () {
-
-    },
-
-    create: function () {
-
-    },
-
-    update: function () {
-
-    },
-};
-
-var game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, 'gameDiv');
-game.state.add(states.game, gameState);
-game.state.start(states.game);
 
 var Asteroids = React.createClass({
   displayName: 'Asteroids',
