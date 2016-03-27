49c6eefc9f37c7200dba6994b82af0d6fc687beb
avocadojesus <jgarbey@gmail.com>
2016-03-26 19:41:30 -0700

    removes dead code

--- public/app/commands/index.js
+++ public/app/commands/index.js
@@ -1,10 +1,3 @@
 var bulk = require('bulk-require')
 var commands = bulk(__dirname + '/bin/', './*/index.js')
-var command_list = []
-for (var i in commands) {
-  command_list.push(i)
-}
-
-
 module.exports = commands
-exports.list = command_list
