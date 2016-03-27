62af81d0dc8198032ee0a3bfec53c6ceac87b3f7
avocadojesus <jgarbey@gmail.com>
2016-03-26 20:09:37 -0700

    updates help for where command

--- public/app/commands/bin/where/where.js
+++ public/app/commands/bin/where/where.js
@@ -2,6 +2,13 @@ var config = require('../../../config.js')
 
 exports.name = "where"
 exports.description = "Discloses the location of the meetup"
-exports.execute = function() {
+exports.execute = function(args) {
+  if (expectOption('-h', args) || expectOption('--help', args)) return this.help()
   return config.address
+}
+exports.help = function() {
+  return [
+    <Ascii key={0} value={this.name} font='bell'/>,
+    <Markdown file={'/app/commands/bin/' + this.name + '/help.md'}/>
+  ]
 }
