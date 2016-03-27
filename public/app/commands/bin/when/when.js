be68806bee40c009d77d7789fbca8eab6eb6b0fe
avocadojesus <jgarbey@gmail.com>
2016-03-26 20:08:47 -0700

    updates help for when command

--- public/app/commands/bin/when/when.js
+++ public/app/commands/bin/when/when.js
@@ -1,6 +1,20 @@
+var React = require('react')
+var $ = window.jQuery = window.$ = require('jquery')
+var Ascii = require('../../../components/ascii')
+var Markdown = require('../../../components/markdown')
+var expectOption = require('../../../lib/command-helper').expectOption
+var expectOptionWithArgument = require('../../../lib/command-helper').expectOptionWithArgument
 var config = require('../../../config.js')
+
 exports.name = "when"
 exports.description = "Discloses the times for the meetup"
-exports.execute = function() {
+exports.execute = function(args) {
+  if (expectOption('-h', args) || expectOption('--help', args)) return this.help()
   return config.time
+}
+exports.help = function() {
+  return [
+    <Ascii key={0} value={this.name} font='bell'/>,
+    <Markdown file={'/app/commands/bin/' + this.name + '/help.md'}/>
+  ]
 }
