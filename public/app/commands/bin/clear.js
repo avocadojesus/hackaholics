4b63f637411d0fb52419aa8caab0654d9135d425
avocadojesus <jgarbey@gmail.com>
2016-03-26 19:09:50 -0700

    resets all commands to folder structure

--- public/app/commands/bin/clear.js
+++
@@ -1,7 +0,0 @@
-var CommandActions = require('../../actions/command-actions')
-
-exports.description = 'Clears your console window'
-exports.name = 'clear'
-exports.execute = function() {
-  CommandActions.deleteAll()
-}
---
+++ public/app/commands/bin/clear/clear.js
@@ -0,0 +1,19 @@
+var React = require('react')
+var $ = window.jQuery = window.$ = require('jquery')
+var Ascii = require('../../components/ascii')
+var CommandActions = require('../../actions/command-actions')
+var expectOption = require('../../lib/command-parser').expectOption
+var expectOptionWithArgument = require('../../lib/command-parser').expectOptionWithArgument
+
+exports.description = 'Clears your console window'
+exports.name = 'clear'
+exports.execute = function(args) {
+  if (expectOption('-h', args) || expectOption('--help', args)) return this.help()
+  CommandActions.deleteAll()
+}
+exports.help = function() {
+  return [
+    <Ascii key={0} value='clear' font='bell'/>,
+    <Markdown file='/markdown/commands/manifesto.md'/>
+  ]
+}
