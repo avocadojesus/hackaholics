caf307c579005d8028cceae976277f8fc85e4422
avocadojesus <jgarbey@gmail.com>
2016-04-02 19:23:22 -0700

    configures socket io to broadcast simple test event

--- config/application.js
+++ config/application.js
@@ -1,3 +1,4 @@
 module.exports = {
-  port: 3069
+  port: 3069,
+  socket_port: 3070
 }
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,6 @@
 var config = require('./application')
-var io = require('socket.io')
 var port = config.port || 3069
+var socket_port = config.socket_port || 3070
 var PagesController = require('../app/controllers/pages')
 var GithubController = require('../app/controllers/github')
 
@@ -8,15 +8,16 @@ exports.init = function(app) {
   app.post('/github', function(req, res) {
     GithubController.handleWebhook(req, res)
   })
-  app.get('*', function(req, res) {
+  app.get('/', function(req, res) {
     return PagesController.home(req, res)
   });
+}
 
-  io(app)
+exports.initSocket = function(io, http) {
   io.on('connection', function(socket) {
-    console.log('a user connected')
+    io.emit('/chat-message', 'hello birld')
   })
 
-  app.listen(port);
+  http.listen(port)
   console.log('Hackaholics is waiting on ' + port)
 }
