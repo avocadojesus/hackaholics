10c827dd104c386bde460478bccad6e02b491d46
avocadojesus <jgarbey@gmail.com>
2016-04-02 19:07:44 -0700

    removes dead packages

--- app/controllers/github.js
+++ app/controllers/github.js
@@ -1,8 +1,6 @@
-var GithubAdaptor = require('../adaptors/github')
-var Git = require('github')
 var shell = require('shelljs')
 
-exports.handleWebhook = function(req, res) { 
+exports.handleWebhook = function(req, res) {
   shell.exec('git pull origin development', function(code, output) {
     console.log(code, output)
     shell.exec('gulp build', function(code, output) {
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,5 @@
 var config = require('./application')
+var io = require('socket.io')
 var port = config.port || 3069
 var PagesController = require('../app/controllers/pages')
 var GithubController = require('../app/controllers/github')
@@ -10,6 +11,11 @@ exports.init = function(app) {
   app.get('*', function(req, res) {
     return PagesController.home(req, res)
   });
+
+  io(app)
+  io.on('connection', function(socket) {
+    console.log('a user connected')
+  })
 
   app.listen(port);
   console.log('Hackaholics is waiting on ' + port)
