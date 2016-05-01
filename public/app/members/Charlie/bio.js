c96ac4be2e51144781b50343afda1d36c7d7ba7f
charlie <charlie@rentageekla.com>
2016-05-01 13:57:54 -0700

    changed charlie bio text.  changed its' to its

--- public/app/members/Charlie/bio.js
+++ public/app/members/Charlie/bio.js
@@ -3,6 +3,6 @@ var React = require('react')
 module.exports = React.createClass({
     displayName: 'Charlie',
     render: function() {
-        return <h1>hi this is a react component</h1>;
+        return <h1>Carrots used to be <a href="http://www.todayifoundout.com/index.php/2010/04/carrots-used-to-be-purple-before-the-17th-century/">purple</a></h1>;
     }
 });
